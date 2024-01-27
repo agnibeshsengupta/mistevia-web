6716affa54fc3a79a01071c7664c1a87e3a825bf
animesh <kpsmiling92@gmail.com>
2024-01-27 19:36:31 +0530

    disclaimer issue fix

--- src/components/home/HeroSection.js
+++ src/components/home/HeroSection.js
@@ -8,12 +8,12 @@ function HeroSection() {
             <Image src="/assets/images/HeroImage.jpg" width="100%" className='object-contain' fill
                 alt="bg"
             />
-            <div className="w-full z-20 absolute bottom-0 px-2 sm:py-6 md:py-16">
+            <div className="w-full z-20 absolute bottom-0 px-1 py-1 sm:py-10 xl:py-20">
                 <div className="flex flex-col items-center justify-center">
-                    <p className="text-xl sm:text-2xl md:text-5xl lg:text-7xl font-extrabold tracking-widest text-gray-dark">
+                    <p className="text-lg sm:text-2xl md:text-5xl lg:text-7xl font-extrabold tracking-widest text-gray-dark">
                         Mistevia
                     </p>
-                    <p className="text-sm sm:text-xl md:text-2xl lg:text-3xl text-bluedark mx-6"> A Culinary Symphony of Healthy and Sugar-Free Delights</p>
+                    <p className="text-xs sm:text-base md:text-2xl lg:text-3xl text-bluedark mx-2"> A Culinary Symphony of Healthy and Sugar-Free Delights</p>
                 </div>
             </div>
 
--- src/components/home/MisteviaMenu.js
+++ src/components/home/MisteviaMenu.js
@@ -8,19 +8,10 @@ const MisteviaMenu = () => {
     return (
         <section className="text-gray-600 body-font overflow-hidden bg-gray-dark h-4/6">
             <div className="container px-5 py-16 mx-auto">
-                <div className='flex'>
-                    <h2 className="font-medium title-font mt-4 text-white text-lg sm:text-2xl md:text-5xl">Our Menu</h2>
-                    <Tooltip
-                        key="disclaimer-tool-tip"
-                        placement="right-start"
-                        content="Product images are for illustrative purposes only and may differ from the actual product"
-                        color="warning"
-                    >
-                        <Info color="#ffa970" />
-                    </Tooltip>
-                </div>
+                <h2 className="font-medium title-font mt-4 text-white text-lg sm:text-2xl md:text-5xl">Our Menu</h2>
                 <div className="w-60 h-1 bg-yellow rounded mt-2 mb-4"></div>
                 <div className='h-4/6'><MenuSlider menuList={MenuList} /></div>
+                <p className=" title-font mt-4 text-white text-lg sm:text-xl md:text-lg">* Product images are for illustrative purposes only and may differ from the actual product</p>
             </div>
         </section >
     );
